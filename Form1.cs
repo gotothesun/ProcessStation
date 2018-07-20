c71738937a5994dc794d0e954157b6acb00d634b
gotothesun <41115516+gotothesun@users.noreply.github.com>
2018-07-20 21:52:23 +0800

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Threading;
+
+namespace ProcessStation
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private string strPath = "";
+        private string strName = "";
+        private bool bAscending = true;
+
+        public class ListViewItemComparer:System.Collections.IComparer
+        {
+            private int col;
+            private SortOrder order;
+
+            public ListViewItemComparer()
+            {
+                col = 0;
+                order = SortOrder.Ascending;
+            }
+
+
+            public ListViewItemComparer(int column, SortOrder order)
+            {
+                col = column;
+                this.order = order;
+            }
+
+
+            public int Compare(object x, object y)
+            {
+                int returnVal = -1;
+                float a = 0, b = 0;
+                if(float.TryParse(((ListViewItem)x).SubItems[col].Text, out a) && float.TryParse(((ListViewItem)y).SubItems[col].Text, out b))
+                {
+                    returnVal = a >= b ? (a == b ? 0 : 1) : -1;
+                }
+                else
+                {
+                    returnVal = String.Compare(((ListViewItem)x).SubItems[col].Text, ((ListViewItem)y).SubItems[col].Text);
+                }
+                if (order == SortOrder.Descending)
+                {
+                    returnVal *= -1;
+                }
+                return returnVal;
+            }
+        }
+
+        private int RefreshView()
+        {
+            Process[] ps = Process.GetProcesses();
+            listProcess.Items.Clear();
+            listProcess.BeginUpdate();
+            foreach (Process p in ps)
+            {
+                ListViewItem lvi = new ListViewItem();
+                lvi.Text = p.ProcessName;
+                lvi.SubItems.Add(p.Id.ToString());
+                try
+                {
+                    lvi.SubItems.Add(p.MainModule.FileName);
+                    listProcess.Items.Add(lvi);
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine(p.ProcessName,ex.Message);
+                }
+            }
+            listProcess.EndUpdate();
+            return ps.Count();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int iTotalProcess = RefreshView();
+            int iLoadedProcess = listProcess.Items.Count;
+            toolStripStatusLabel3.Text = "系统目前正在运行的进程数为：" + iLoadedProcess.ToString() + "/" + iTotalProcess.ToString();
+            
+        }
+
+        private void listProcess_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int iTotalProcess = RefreshView();
+            int iLoadedProcess = listProcess.Items.Count;
+            toolStripStatusLabel3.Text = "系统目前正在运行的进程数为：" + iLoadedProcess.ToString() + "/" + iTotalProcess.ToString();
+
+        }
+
+        private void buttonKill_Click(object sender, EventArgs e)
+        {
+            if (listProcess.SelectedItems.Count == 0)
+                return;
+            string ProcessID = listProcess.SelectedItems[0].SubItems[1].Text;
+            Process p = Process.GetProcessById(int.Parse(ProcessID));
+            p.Kill();
+            int i = listProcess.SelectedItems[0].Index;
+            listProcess.Items[i].Remove();
+        }
+
+        private void listProcess_ColumnClick(object sender, ColumnClickEventArgs e)
+        {
+            if (bAscending)
+            {
+                listProcess.ListViewItemSorter = new ListViewItemComparer(e.Column, SortOrder.Ascending);
+                bAscending = false;
+            }
+            else
+            {
+                listProcess.ListViewItemSorter = new ListViewItemComparer(e.Column, SortOrder.Descending);
+                bAscending = true;
+            }
+            listProcess.Sort();
+        }
+
+        private void buttonProtect_Click(object sender, EventArgs e)
+        {
+            if (listProcess.SelectedItems.Count == 0)
+                MessageBox.Show("请选中你要进行保护的进程！");
+            else
+            {
+                timer1.Start();
+                strName = listProcess.SelectedItems[0].Text;
+                strPath = listProcess.SelectedItems[0].SubItems[2].Text;
+                toolStripStatusLabel1.Text = "正在保护进程：" + strName;
+                Debug.WriteLine(strPath);
+            }
+                
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Process[] ps = Process.GetProcessesByName(strName);
+            int iTotalProcess = RefreshView();
+            int iLoadedProcess = listProcess.Items.Count;
+            toolStripStatusLabel3.Text = "系统目前正在运行的进程数为：" + iLoadedProcess.ToString() + "/" + iTotalProcess.ToString();
+            foreach (ListViewItem item in listProcess.Items)
+            {
+                if (item.SubItems[0].Text.Equals(strName))
+                {
+                    Debug.WriteLine("请放心，进程安在。");
+                    return;
+                }
+
+            }
+            Process p = new Process();
+            p.StartInfo.FileName = strPath;
+            p.Start();
+            Debug.WriteLine("进程不在，已重新启动！");
+        }
+    }
+}
