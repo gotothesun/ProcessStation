38f87e860429a4bc8b2af35a82a7e093497d3823
gotothesun <41115516+gotothesun@users.noreply.github.com>
2018-07-21 17:16:51 +0800

    Update Form1.Designer.cs

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace ProcessStation
+namespace ProcessStation
 {
     partial class Form1
     {
@@ -41,7 +41,12 @@
             this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
             this.toolStripStatusLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
             this.toolStripStatusLabel3 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.buttonPrevent = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
             this.statusStrip1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // listProcess
@@ -53,11 +58,10 @@
             this.columnHeader2,
             this.columnHeader3});
             this.listProcess.FullRowSelect = true;
-            this.listProcess.Location = new System.Drawing.Point(15, 15);
-            this.listProcess.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.listProcess.Location = new System.Drawing.Point(9, 9);
             this.listProcess.MultiSelect = false;
             this.listProcess.Name = "listProcess";
-            this.listProcess.Size = new System.Drawing.Size(671, 366);
+            this.listProcess.Size = new System.Drawing.Size(558, 433);
             this.listProcess.TabIndex = 2;
             this.listProcess.UseCompatibleStateImageBehavior = false;
             this.listProcess.View = System.Windows.Forms.View.Details;
@@ -66,8 +70,8 @@
             // 
             // columnHeader1
             // 
-            this.columnHeader1.Text = "  进程名称";
-            this.columnHeader1.Width = 200;
+            this.columnHeader1.Text = " 进程名称";
+            this.columnHeader1.Width = 150;
             // 
             // columnHeader2
             // 
@@ -80,10 +84,9 @@
             // 
             // buttonUpdate
             // 
-            this.buttonUpdate.Location = new System.Drawing.Point(49, 423);
-            this.buttonUpdate.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.buttonUpdate.Location = new System.Drawing.Point(21, 12);
             this.buttonUpdate.Name = "buttonUpdate";
-            this.buttonUpdate.Size = new System.Drawing.Size(146, 42);
+            this.buttonUpdate.Size = new System.Drawing.Size(89, 33);
             this.buttonUpdate.TabIndex = 3;
             this.buttonUpdate.Text = "立即更新";
             this.buttonUpdate.UseVisualStyleBackColor = true;
@@ -91,10 +94,9 @@
             // 
             // buttonKill
             // 
-            this.buttonKill.Location = new System.Drawing.Point(495, 426);
-            this.buttonKill.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.buttonKill.Location = new System.Drawing.Point(138, 14);
             this.buttonKill.Name = "buttonKill";
-            this.buttonKill.Size = new System.Drawing.Size(132, 39);
+            this.buttonKill.Size = new System.Drawing.Size(89, 33);
             this.buttonKill.TabIndex = 4;
             this.buttonKill.Text = "关闭进程";
             this.buttonKill.UseVisualStyleBackColor = true;
@@ -102,10 +104,9 @@
             // 
             // buttonProtect
             // 
-            this.buttonProtect.Location = new System.Drawing.Point(284, 423);
-            this.buttonProtect.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.buttonProtect.Location = new System.Drawing.Point(145, 12);
             this.buttonProtect.Name = "buttonProtect";
-            this.buttonProtect.Size = new System.Drawing.Size(132, 39);
+            this.buttonProtect.Size = new System.Drawing.Size(89, 33);
             this.buttonProtect.TabIndex = 5;
             this.buttonProtect.Text = "保护进程";
             this.buttonProtect.UseVisualStyleBackColor = true;
@@ -123,25 +124,25 @@
             this.toolStripStatusLabel1,
             this.toolStripStatusLabel2,
             this.toolStripStatusLabel3});
-            this.statusStrip1.Location = new System.Drawing.Point(0, 484);
+            this.statusStrip1.Location = new System.Drawing.Point(0, 509);
             this.statusStrip1.Name = "statusStrip1";
-            this.statusStrip1.Padding = new System.Windows.Forms.Padding(2, 0, 28, 0);
-            this.statusStrip1.Size = new System.Drawing.Size(701, 36);
+            this.statusStrip1.Padding = new System.Windows.Forms.Padding(1, 0, 16, 0);
+            this.statusStrip1.Size = new System.Drawing.Size(575, 38);
             this.statusStrip1.TabIndex = 6;
             this.statusStrip1.Text = "statusStrip1";
             // 
             // toolStripStatusLabel1
             // 
             this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
-            this.toolStripStatusLabel1.Size = new System.Drawing.Size(230, 31);
-            this.toolStripStatusLabel1.Text = "正在保护进程：暂无";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(265, 33);
+            this.toolStripStatusLabel1.Text = "正在保护进程：暂无     ";
             this.toolStripStatusLabel1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // toolStripStatusLabel2
             // 
             this.toolStripStatusLabel2.Name = "toolStripStatusLabel2";
-            this.toolStripStatusLabel2.Size = new System.Drawing.Size(230, 31);
-            this.toolStripStatusLabel2.Text = "正在阻止进程：暂无";
+            this.toolStripStatusLabel2.Size = new System.Drawing.Size(265, 33);
+            this.toolStripStatusLabel2.Text = "正在阻止进程：暂无     ";
             this.toolStripStatusLabel2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // toolStripStatusLabel3
@@ -150,23 +151,58 @@
             this.toolStripStatusLabel3.Size = new System.Drawing.Size(398, 31);
             this.toolStripStatusLabel3.Text = "系统目前正在运行的进程数为：未知";
             // 
+            // buttonPrevent
+            // 
+            this.buttonPrevent.Location = new System.Drawing.Point(16, 14);
+            this.buttonPrevent.Margin = new System.Windows.Forms.Padding(2);
+            this.buttonPrevent.Name = "buttonPrevent";
+            this.buttonPrevent.Size = new System.Drawing.Size(89, 33);
+            this.buttonPrevent.TabIndex = 7;
+            this.buttonPrevent.Text = "阻止进程";
+            this.buttonPrevent.UseVisualStyleBackColor = true;
+            this.buttonPrevent.Click += new System.EventHandler(this.buttonPrevent_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.buttonPrevent);
+            this.panel1.Controls.Add(this.buttonKill);
+            this.panel1.Location = new System.Drawing.Point(313, 451);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(254, 59);
+            this.panel1.TabIndex = 8;
+            // 
+            // panel2
+            // 
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Controls.Add(this.buttonProtect);
+            this.panel2.Controls.Add(this.buttonUpdate);
+            this.panel2.Location = new System.Drawing.Point(11, 451);
+            this.panel2.Margin = new System.Windows.Forms.Padding(2);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(254, 59);
+            this.panel2.TabIndex = 9;
+            // 
             // Form1
             // 
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
-            this.ClientSize = new System.Drawing.Size(701, 520);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 28F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(575, 547);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.statusStrip1);
-            this.Controls.Add(this.buttonProtect);
-            this.Controls.Add(this.buttonKill);
-            this.Controls.Add(this.buttonUpdate);
             this.Controls.Add(this.listProcess);
+            this.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
-            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.Text = "ProcessStation";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.statusStrip1.ResumeLayout(false);
             this.statusStrip1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -185,6 +221,8 @@
         private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
         private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel2;
         private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel3;
+        private System.Windows.Forms.Button buttonPrevent;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
     }
 }
-
